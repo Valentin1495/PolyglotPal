1084eb92a8979c721b962c4946656267a92d7a0c
Valentin1495 <newface95@icloud.com>
2023-12-15 20:35:57 +0900

    refactor: reset

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
 import './globals.css';
-import Header from '@/components/header';
 import { ThemeProvider } from '@/components/theme-provider';
 
-const inter = Inter({ subsets: ['latin'] });
+const font = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,14 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={font.className}>
         <ThemeProvider
           attribute='class'
           defaultTheme='system'
           enableSystem
           disableTransitionOnChange
         >
-          <Header />
           {children}
         </ThemeProvider>
       </body>
--- components/sign-in-button.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from './ui/button';
-
-export default function SignInButton() {
-  return <Button variant='ghost'>Sign in</Button>;
-}
