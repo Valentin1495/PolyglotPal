7edff148a3ffcf4f25bfacd09ea3952ffba08992
Valentin1495 <newface95@icloud.com>
2024-02-23 16:20:12 +0900

    Modify dialog

--- src/components/Dialog/dialog.tsx
+++ src/components/Dialog/dialog.tsx
@@ -19,70 +19,55 @@ export default function Dialog() {
         justifyContent: 'center',
       }}
     >
-      <button
-        onClick={openDialog}
-        className='dialog-trigger-btn'
-        style={{
-          zIndex: open ? '0' : '10',
-        }}
-      >
+      <button onClick={openDialog} className='dialog-trigger-btn'>
         Show Dialog
       </button>
-
-      <section
-        style={{
-          position: 'fixed',
-          inset: 0,
-          backgroundColor: 'rgba(0, 0, 0, 0.3)',
-          opacity: open ? '1' : '0',
-          transition: 'opacity 300ms ease-in-out',
-        }}
-        onClick={closeDialog}
-      ></section>
-      <section
-        style={{
-          backgroundColor: 'white',
-          position: 'fixed',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          borderRadius: '8px',
-          textAlign: 'left',
-          padding: '0 20px 20px',
-          opacity: open ? '1' : '0',
-          transition: 'opacity 100ms ease-in-out',
-        }}
-      >
-        <h3>Are you absolutely sure?</h3>
-        <p
-          style={{
-            fontSize: '0.9rem',
-          }}
-        >
-          This action cannot be undone. This will permanently delete your
-          account and remove your data from our servers.
-        </p>
-        <article
-          style={{
-            textAlign: 'right',
-          }}
-        >
-          <button
-            className='dialog-left-btn'
+      {open && (
+        <div className='dialog'>
+          <section
+            style={{
+              position: 'fixed',
+              inset: 0,
+              backgroundColor: 'rgba(0, 0, 0, 0.3)',
+            }}
             onClick={closeDialog}
-            disabled={!open}
+          ></section>
+          <section
+            style={{
+              backgroundColor: 'white',
+              position: 'fixed',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              borderRadius: '8px',
+              textAlign: 'left',
+              padding: '0 20px 20px',
+            }}
           >
-            Cancel
-          </button>
-          <button
-            className='dialog-right-btn'
-            onClick={closeDialog}
-            disabled={!open}
-          >
-            Continue
-          </button>
-        </article>
-      </section>
+            <h3>Are you absolutely sure?</h3>
+            <p
+              style={{
+                fontSize: '0.9rem',
+              }}
+            >
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </p>
+            <article
+              style={{
+                textAlign: 'right',
+              }}
+            >
+              <button className='dialog-left-btn' onClick={closeDialog}>
+                Cancel
+              </button>
+              <button className='dialog-right-btn' onClick={closeDialog}>
+                Continue
+              </button>
+            </article>
+          </section>
+        </div>
+      )}
     </div>
   );
 }
